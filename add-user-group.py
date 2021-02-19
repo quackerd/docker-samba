1f4114160d1536b5159daadd0c8634bdee7243cf
quackerd <d@quacker.org>
2021-02-19 18:26:08 -0500

    change example

--- add-user-group.py
+++ add-user-group.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import subprocess
+import shlex
 
 def main():
     if (len(sys.argv) < 3):
@@ -18,7 +19,7 @@ def main():
         if (len(elements) != 2):
             print("Skipping invalid group config string \"" + group + "\"")
             continue
-        subprocess.check_call("addgroup -g " + elements[1] + " " + elements[0], shell=True)
+        subprocess.check_call(shlex.join(["addgroup", "-g", elements[1], elements[0]]), shell=True)
         print("Added group " + elements[0] + " with gid " + elements[1])
         
 
@@ -28,13 +29,13 @@ def main():
         if (len(elements) != 3 and len(elements) != 4):
             print("Skipping invalid user config string \"" + user + "\"")
             continue
-        subprocess.check_call("adduser -D -H -u " + elements[1] + " " + elements[0], shell=True)
+        subprocess.check_call(shlex.join(["adduser", "-D", "-H", "-u", elements[1], elements[0]]), shell=True)
         print("Added user " + elements[0] + " with uid " + elements[1])
         if (len(elements) == 4):
-            subprocess.check_call("addgroup " + elements[0] + " " + elements[3], shell=True)
+            subprocess.check_call(shlex.join(["addgroup", elements[0], elements[3]]), shell=True)
             print("Added user " + elements[0] + " to group " + elements[3])
         # set passwd
-        subprocess.check_call("echo -ne \"" + elements[2] + "\n" + elements[2] + "\n" + "\" | smbpasswd -a -U " + elements[0], shell=True)
+        subprocess.check_call(shlex.join(["echo", "-ne", "\"" + elements[2] + "\n" + elements[2] + "\n\"", "|", "smbpasswd", "-a", "-U", elements[0]]), shell=True)
         print("Set user " + elements[0] + " password to " + elements[2])
 
 main()
