fa0bf83d485642e0b9eb4a557d4482c80ee4df46
quackerd <d@quacker.org>
2021-02-19 17:36:13 -0500

    change example

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "====== Configuring users and groups ====="
 python3 /opt/add-user-group.py "$GROUPS" "$USERS"
 echo ""
 echo "====== Starting Samba Daemon ====="
-exec smbd -S -F -d 2 --no-process-group -s /samba/smb.conf
+exec smbd -S -F --no-process-group -s /samba/smb.conf $(echo $SMBD_ARGS)
